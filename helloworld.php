6627016a0f4ee547f1430e60e99dcb4f94bbdc10
maurits.bakker@edu-kw1c.nl <maurits.bakker@edu-kw1c.nl>
2020-02-03 16:13:27 +0100

    hello word

---
+++ helloworld.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+            <?php print "pagina1"; ?>
+        </title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            echo "hello world";
+            ?>
+        </h1>
+    </body>
+</html>
