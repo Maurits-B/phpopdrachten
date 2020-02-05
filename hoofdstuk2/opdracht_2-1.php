1f194e94a78dc3eef1a6516168b50bb66e62cfa4
maurits.bakker@edu-kw1c.nl <maurits.bakker@edu-kw1c.nl>
2020-02-05 14:06:44 +0100

    Opdracht 2.1

---
+++ hoofdstuk2/opdracht_2-1.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: Maurits Bakker
+ * Date: 5-2-2020
+ * Time: 13:50
+ * File: opdracht_2-1.php
+ */
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta name="description" content=" ">
+        <meta name="keywords" content=" ">
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="CSS\style.css">
+        <title>
+            Opdracht X
+        </title>
+    </head>
+    <body>
+        <header>
+            <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+        </header>
+        <p>
+            <?php
+            echo "hello world";
+            ?>
+        </p>
+    </body>
+</html>
