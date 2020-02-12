0976a81a7dace667013f48f5b4af9cc58f646230
Maurits Bakkker <mauritsbakkerbreda@gmail.com>
2020-02-12 13:57:56 +0100

    opdracht 2.2 heel.2

---
+++ hoofdstuk2/insructies/oefening1.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * User: Maurits Bakker
+ * Date: Date
+ * Time: **:** AM/PM
+ * File: opdracht_2-1.php
+ */
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta name="description" content=" ">
+        <meta name="keywords" content=" ">
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="CSS\style.css">
+        <title>d
+            index
+        </title>
+    </head>
+    <body>
+
+    <header>
+        <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    </header>
+
+    <aside>
+        <?php
+            //string
+            $car = "audi";
+
+            //Boolean
+            $license = true;
+
+            //intenger
+            $age = 24;
+
+            //Float
+            $price = 22000.99;
+
+            echo $car. " heeft een prijs van: " . $price;
+        ?>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+    </main>
+    </body>
+</html>
+
