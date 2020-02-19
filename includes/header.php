dd920f91a92cf4e1c12578e04c1832d10c6932b3
Maurits Bakkker <mauritsbakkerbreda@gmail.com>
2020-02-19 13:53:51 +0100

    includes toevoegen

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * User: Maurits Bakker
+ * Date: 19-2-2020
+ * Time: 01:30 AM
+ * File: header.php
+ */
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta name="description" content=" ">
+        <meta name="keywords" content=" ">
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="/phpopdrachten/CSS/style.css">
+        <title>
+            index
+        </title>
+    </head>
+    <body>
+        <header>
+            <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+        </header>
---
+++ includes/menu.php
@@ -0,0 +1,52 @@
+<?php
+$base_url = "http://localhost/phpopdrachten/";
+?>
+
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="/phpopdrachten/hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="/phpopdrachten/hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 3
+            <ul>
+                <li>
+                    <a href="/phpopdrachten/hoofdstuk3/opdracht_3-1.php">Opdracht 3.1</a>
+                </li>
+                <li>
+                    <a href="/phpopdrachten/hoofdstuk3/opdracht_3-2.php">Opdracht 3.2</a>
+                </li>
+                <li>
+                    <a href="/phpopdrachten/hoofdstuk3/opdracht_3-3.php">Opdracht 3.3</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 4
+            <ul>
+                <li>
+                    <a href="/phpopdrachten/hoofdstuk4/opdracht_4-1.php">Opdracht 4.1</a>
+                </li>
+                <li>
+                    <a href="/phpopdrachten/hoofdstuk4/opdracht_4-2.php">Opdracht 4.2</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 5
+            <ul>
+                <li>
+                    <a href="/phpopdrachten/hoofdstuk5/opdracht_5-1.php">Opdracht 5.1</a>
+                </li>
+                <li>
+                    <a href="/phpopdrachten/hoofdstuk5/opdracht_5-2.php">Opdracht 5.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
