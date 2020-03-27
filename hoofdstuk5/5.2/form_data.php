045a342b06a3f54ad97156bbe396c8dc4b148de3
Maurits <57364928+Maurits-B@users.noreply.github.com>
2020-03-27 10:29:17 +0100

    5.2

--- hoofdstuk5/5.2/form_data.php
+++ hoofdstuk5/5.2/form_data.php
@@ -1,12 +1,91 @@
-<?php
-print_r($_GET);
+<html>
+    <head>
 
+    </head>
+    <body>
+    <?php
+    print_r($_GET);
+    ?>
+        <h1>Uitschrijf Formulier</h1>
+        <hr>
+        <table>
+            <tr>
+                <td>Naam</td>
+                <td>
+                    <?php
+                        echo $_GET["name"];
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Student nummer</td>
+                <td>
+                    <?php
+                       echo $_GET["studentid"];
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Datum van uitschrijving</td>
+                <td>
+                    <?php
+                         echo $_GET["date"];
+                     ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Rede van uitschrijving</td>
+                <td>
+                    <?php
+                        echo $_GET["reason"];
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Leerjaar</td>
+                <td>
+                    <?php
+                        echo $_GET["year"];
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Succesklas</td>
+                <td>
+                    <?php
+                        if ($_GET["succesklas"] == "on")
+                        {
+                            echo "Ja";
+                        }
+                        else
+                        {
+                            echo "Er is niet aangemeld voor de Succesklas.";
+                        }
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Verwijder gegevens</td>
+                <td>
+                    <?php
+                        if ($_GET["delinformation"] == "on")
+                        {
+                            echo "Ja";
+                        }
+                        else
+                        {
+                            echo "Gegevens hoeven niet worden verwijderd.";
+                        }
+                    ?>
+                </td>
+            </tr>
+        </table>
+         <p>
+            Bericht <br>
+             <?php
+                echo $_GET["message"];
+             ?>
+        </p>
+    </body>
+</html>
 
-echo "<table class='eenvoudig'>";
-echo "<tr><td>Docent</td><td>".$_GET["teacherName"]."</td></tr>";
-echo "<tr><td>Vak</td><td>".$_GET["course"]."</td></tr>";
-echo "<tr><td>Is het
-moeilijk</td><td>".$_GET["isDifficult"]."</td></tr>";
-echo "<tr><td>Is het nuttig</td><td>".$_GET["isUseful"]."</td></tr>";
-echo "</table>";
-?>
