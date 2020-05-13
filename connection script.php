dc98c6e95681fa3b1c899587253a734c448f9d0b
Maurits Bakkker <mauritsbakkerbreda@gmail.com>
2020-05-13 13:52:55 +0200

    connection done

--- connection script.php
+++ connection script.php
@@ -1,4 +1,5 @@
 <?php
+//open de database conecctie en de EDBC driver
 try {
     $pdo = new PDO ("odbc:odbc2sqlserver");
 }
@@ -10,3 +11,32 @@ catch (PDOException $e)
 }
 
 echo "database connectie is gelukt";
+
+//Uitvoeren van een SQL query
+try {
+    //Query scrhijven
+    $sql = 'SELECT * FROM joke';
+
+    //Query uitvoeren
+    $result = $pdo->query($sql);
+}
+catch (PDOException $e)
+{
+    echo 'Er is een probleem met het ophalen van jokes: ' . $e->getMessage();
+    exit();
+}
+
+//Lege Array aanmaken voor de results
+$aJokes = array();
+
+//Doo de results heen loopem via een while
+while ($row = $result->fetch(PDO::FETCH_ASSOC))
+{
+    //Result wegschrijven in de $aJokes array
+    $aJokes[] = $row;
+}
+
+//tonen van de inhoud van aJokes
+echo "<pre>";
+var_dump($aJokes);
+echo "</pre>";
