94be77cb0f3c476bc9fbefd53d5c0594f83598e9
Maurits <57364928+Maurits-B@users.noreply.github.com>
2020-06-15 21:50:42 +0200

    Sorry

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ include "includes/menu.php";
 include "includes/variabelen.php";
 ?>
 <main id="wrapper">
-    <h2>Uitwerkingen</h2>
-
+    <h2>ZEEBATS</h2>
+    <iframe width="1000" height="700" src="https://www.youtube.com/embed/Zug3kOsbTnU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </main>
 <?php
 include "includes/footer.php";
